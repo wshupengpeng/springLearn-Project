cfff662488a42c8e4f0b32eb4ccbbf84cd6a00d2
hpp <m18602830302@163.com>
2022-10-12 00:03:08 +0800

    添加依赖

--- springProject/src/main/java/com/spring/excel/aop/ExportExcelAop.java
+++ springProject/src/main/java/com/spring/excel/aop/ExportExcelAop.java
@@ -1,10 +1,12 @@
 package com.spring.excel.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+
 /**
  * @creater hpp
  * @Date 2022/10/9-23:17
  * @description:
  */
-
+@Aspect
 public class ExportExcelAop {
 }
